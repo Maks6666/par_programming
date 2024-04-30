38e3c9aec02c7785ccf94d7e1bee775a3786d919
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-30 21:06:08 +0300

    3rd task

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-# Користувач вводить з клавіатури значення у список.
-# Після чого запускаються два потоки.
-# Перший потік знаходить суму елементів у списку. Другий потік знаходить
-# середнє арифметичне у списку. Результати обчислень
-# виведіть на екран.
+# Користувач вводить з клавіатури шлях до файлу, що
+# містить набір чисел. Після чого запускаються два потоки.
+# Перший потік створює новий файл, в який запише лише
+# парні елементи списку. Другий потік створює новий файл,
+# в який запише лише непарні елементи списку. Кількість
+# парних і непарних елементів виводиться на екран.
 
 import threading
+import pickle
 
-def summa(lst):
-    res = 0
-    for i in lst:
-        res += i
-    print(res)
+def create_file():
+    file_name = input("Input file name: ")
+    data = []
+    numbers = int(input("Input length of the list: "))
+    for i in range(numbers):
+        num = int(input(f"Input {i+1}st number: "))
+        data.append(num)
+    with open(f"{file_name}.pickle", "wb") as file:
+        pickle.dump(data, file)
+    return "Data saved."
 
-def medium(lst):
-    res = 0
-    for i in lst:
-        res += i
-
-    mid = res / len(lst)
-    print(mid)
-
-def create_list(num):
-    lst = []
-    for i in range(num):
-        number = int(input(f"Input {i+1}st number: "))
-        lst.append(number)
-    t1 = threading.Thread(target=summa, args=(lst,))
-    t2 = threading.Thread(target=medium, args=(lst,))
-
-    t1.start()
-    t2.start()
-
-    return t1, t2
+def even_numbers(file_name, new_file_name):
+    with open(file_name, "rb") as file:
+        read_data = pickle.load(file)
+    if len(read_data) > 0:
+        even_numbers_list = []
+        for i in read_data:
+            if i % 2 == 0:
+                even_numbers_list.append(i)
+        with open(f"{new_file_name}.pickle", "wb") as file:
+            pickle.dump(even_numbers_list, file)
+        print(even_numbers_list)
 
 
-print(create_list(4))
+def non_even_numbers(file_name, new_file_name):
+    with open(file_name, "rb") as file:
+        read_data = pickle.load(file)
+    if len(read_data) > 0:
+        non_even_numbers_list = []
+        for i in read_data:
+            if i % 2 != 0:
+                non_even_numbers_list.append(i)
+        with open(f"{new_file_name}.pickle", "wb") as file:
+            pickle.dump(non_even_numbers_list, file)
+        print(non_even_numbers_list)
+
+
+create_file()
+t1 = threading.Thread(target=even_numbers, args=('data.pickle', 'even_num_data'))
+t2 = threading.Thread(target=non_even_numbers, args=('data.pickle', 'non_even_num_data'))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
