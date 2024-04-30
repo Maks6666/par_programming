dc69234585a4dfd03771ad6244f7c04b5d9e4649
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-30 20:33:13 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 # Користувач вводить з клавіатури значення у список.
 # Після чого запускаються два потоки.
-# Перший потік знаходить максимум у списку. Другий потік знаходить мінімум
-# у списку. Результати обчислень виведіть на екран.
+# Перший потік знаходить суму елементів у списку. Другий потік знаходить
+# середнє арифметичне у списку. Результати обчислень
+# виведіть на екран.
 
 import threading
 
-def maximum(lst):
-    print(max(lst))
+def summa(lst):
+    res = 0
+    for i in lst:
+        res += i
+    print(res)
 
-def minimum(lst):
-    print(min(lst))
+def medium(lst):
+    res = 0
+    for i in lst:
+        res += i
+
+    mid = res / len(lst)
+    print(mid)
 
 def create_list(num):
     lst = []
     for i in range(num):
         number = int(input(f"Input {i+1}st number: "))
         lst.append(number)
-    t1 = threading.Thread(target=maximum, args=(lst,))
-    t2 = threading.Thread(target=minimum, args=(lst,))
+    t1 = threading.Thread(target=summa, args=(lst,))
+    t2 = threading.Thread(target=medium, args=(lst,))
 
     t1.start()
     t2.start()
